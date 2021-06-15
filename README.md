ced9b5c4ecec3af6e0ad18c57e8f03cf23d6e4dd
Nicholas Napoli <nicknapoli82@gmail.com>
2021-06-14 21:35:20 -0700

    Modified to operate on lists of tiles

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Just a simple Sudoku puzzle solver in c. 
 
-For me, the actual puzzle was secondary to my entertaining the idea of using a function queue can be generalized to take a function and environment, and just run it. So the puzzle itself was just an excuse to impliment that alone. 
+For me, the actual puzzle was secondary to my entertaining the idea of using a function queue can be generalized to take a function and environment, and just run it. So the puzzle itself was just an excuse to implement that alone. 
 
 The function queue was actually simpler to create than I realized though. So ¯\\_(ツ)_/¯
 
--- sudoku.c
+++ sudoku.c
@@ -77,6 +77,77 @@ i8(*sudoku_solvers[])(void *, func_queue *) =
 { solve_possibleSinglehidden, solve_possibleGroupOpen,
   solve_numberLinedUpRow, solve_numberLinedUpColumn };
 
+/*********************************************************/
+/* Handles local scope allocations for whatever you want */
+/* Frees them at end of function			 */
+/* Just pair Allocs_push with Allocs_pop		 */
+/*********************************************************/
+typedef struct s_Points {
+    void **points;
+    u32 cursor;
+    u32 length;
+}s_Points;
+
+typedef struct s_Allocs {
+    s_Points **points;
+    u32 cursor;
+    u32 length;
+}s_Allocs;
+
+s_Allocs Allocs;
+
+void Allocs_push(void) {
+    if ( Allocs.cursor == Allocs.length ) {
+	Allocs.points = realloc(Allocs.points, sizeof(void*) * (Allocs.length + 4));
+	if ( Allocs.points == NULL )
+	    OOM_error();
+	memset(Allocs.points + Allocs.length, 0, sizeof(void *) * 4);
+	Allocs.length += 4;
+    }
+    if ( Allocs.points[0] != NULL )
+	Allocs.cursor++;
+}
+
+void Allocs_pop() {
+    if ( Allocs.points[Allocs.cursor] ) {
+	s_Points *loc = Allocs.points[Allocs.cursor];
+	for (u32 i = 0; i < loc->cursor; i++) {
+	    free(loc->points[i]);
+	    loc->points[i] = NULL;
+	}
+	free(loc->points);
+	loc->cursor = 0;
+	loc->length = 0;
+
+	free(Allocs.points[Allocs.cursor]);
+	Allocs.points[Allocs.cursor] = NULL;
+    }
+    if ( Allocs.cursor == 0 ) {
+	free(Allocs.points);
+	Allocs.points = NULL;
+	Allocs.length = 0;
+    }
+    if ( Allocs.cursor > 0)
+	Allocs.cursor--;
+}
+
+void* Allocs_mem_push(void *a) {
+    if ( Allocs.points[Allocs.cursor] == NULL ) {
+	Allocs.points[Allocs.cursor] = calloc(1, sizeof(s_Points));
+    }
+    s_Points *loc = Allocs.points[Allocs.cursor];
+    if ( loc->cursor == loc->length ) {
+	loc->points = realloc(loc->points, sizeof(void*) * (loc->length + 8));
+	if ( loc->points == NULL )
+	    OOM_error();
+	memset(loc->points + loc->length, 0, sizeof(void *) * 8);
+	loc->length += 8;
+    }
+    loc->points[loc->cursor] = a;
+    loc->cursor++;
+    return a;
+}
+
 int main(void) {
     // Function queue, to just pass around and use
     func_queue *fQueue = calloc(1, sizeof(func_queue));
@@ -188,6 +259,91 @@ i8 dequeue(func_queue *self) {
     return result;
 }
 
+/***********************************************************/
+/* I'm entertaining the idea of just operating on the grid */
+/* as through lists. This code does nothing right now for  */
+/* the program. Its only here as reminder to self that I   */
+/* have this opportunity. 				   */
+/***********************************************************/
+
+typedef struct T_List {
+    u8 x;
+    u8 y;
+    struct tile* tile;
+}T_List;
+
+typedef struct list_tiles {
+    u32 length;
+    T_List *tiles;
+}list_tiles;
+
+// Creates a list of tiles on the heap and returns a pointer
+// List of tiles generated are within the constraints of x and y
+// and applies the function given to determine inclusion
+// Max size of the list should be 9
+list_tiles gen_tile_list(u8 lower_y, u8 upper_y, u8 lower_x, u8 upper_x,
+			 void* discluding, struct tile (*grid)[9]) {
+    T_List list[9] = {0};
+    u8 list_cursor = 0;
+    for (u8 y = lower_y; y < upper_y; y++) {
+	for (u8 x = lower_x; x < upper_x; x++) {
+	    if ( discluding != &grid[y][x] ) {
+		list[list_cursor].y = y;
+		list[list_cursor].x = x;
+		list[list_cursor].tile = &grid[y][x];
+		list_cursor++;
+	    }
+	}
+    }
+
+    T_List *result = NULL;
+    if ( list_cursor > 0 ) {
+	result = calloc(list_cursor, sizeof(T_List) * list_cursor);
+	if ( result == NULL )
+	    OOM_error();
+	memcpy(result, list, sizeof(T_List) * list_cursor);
+    }
+    return (list_tiles){list_cursor, result};
+}
+
+enum TILE_FILTERS { TF_TSN, TF_TN, TF_SN, TF_T, TF_S, TF_N };
+
+list_tiles filter_list_tiles(list_tiles *tiles, struct tile *self, u32 number, enum TILE_FILTERS using, i8 (*f)()) {
+    T_List list[9] = {0};
+    u8 list_cursor = 0;
+    for (u8 i = 0; i < tiles->length; i++) {
+        i8 test = 0;
+        switch (using) {
+        case TF_TSN: { test = f(tiles->tiles[i], self, number); break; }
+        case TF_TN: { test = f(tiles->tiles[i], number); break; }
+        case TF_SN: { test = f(self, number); break; }
+        case TF_T: { test = f(tiles->tiles[i]); break; }
+        case TF_S: { test = f(self); break; }
+        case TF_N: { test = f(number); break; }
+        }
+        if ( test ) {
+            list[list_cursor] = tiles->tiles[i];
+            list_cursor++;
+        }
+    }
+    T_List *result = NULL;
+    if ( list_cursor > 0 ) {
+        result = calloc(list_cursor, sizeof(T_List) * list_cursor);
+        if ( result == NULL )
+            OOM_error();
+        memcpy(result, list, sizeof(T_List) * list_cursor);
+    }
+    return (list_tiles){list_cursor, result};
+}
+
+i8 filter_not_absolute(T_List T) {
+    return !T.tile->absolute;
+}
+
+i8 filter_is_possible(T_List T, u32 number) {
+    return T.tile->possible & (1 << number) ? 1 : 0;
+}
+
 i8 tile_resolved(void *e, func_queue *queue) {
     func_queue *fq = queue;
     tile_env *env = e;
@@ -264,7 +420,6 @@ i8 tile_resolveBroadcast(void *e, func_queue *queue) {
     return 1;
 }
 
-
 /**********************************************************************/
 /* Algorithms that extend beyond simple column, row, box eliminations */
 /* begin here. 							      */
@@ -277,6 +432,7 @@ i8 tile_resolveBroadcast(void *e, func_queue *queue) {
 //     and another with (1, 2, 3) then we know we can
 //     eliminate both 1 and 2 from that additional tile
 i8 solve_possibleSinglehidden(void *e, func_queue *queue) {
+    Allocs_push();
     solvers_env *env = e;
     struct tile (*grid)[9] = env->grid;
 
@@ -284,37 +440,27 @@ i8 solve_possibleSinglehidden(void *e, func_queue *queue) {
 	for (u8 x = 0; x < 9; x++) {
 	    if ( grid[y][x].absolute )
 		continue;
-	    u8 lower_y = y - (y % 3);
-	    u8 upper_y = lower_y + 3;
-	    u8 lower_x = x - (x % 3);
-	    u8 upper_x = lower_x + 3;
-	    u8 number = 0;
-	    for (u8 i = 1; i < 10 && !number; i++) {
-		u8 elsewhere = 0;
+	    list_tiles box_list = gen_tile_list(y - (y % 3), y - (y % 3) + 3, x - (x % 3), x - (x % 3) + 3, &grid[y][x], grid);
+	    Allocs_mem_push(box_list.tiles);
+	    box_list = filter_list_tiles(&box_list, NULL, 0, TF_T, filter_not_absolute);
+	    Allocs_mem_push(box_list.tiles);
+	    for (u8 i = 1; i < 10; i++) {
 		if ( grid[y][x].possible & (1 << i) ) {
-		    number = i;
-		    for (u8 y1 = lower_y; y1 < upper_y && !elsewhere; y1++) {
-			for (u8 x1 = lower_x; x1 < upper_x && !elsewhere; x1++) {
-			    if ( !grid[y1][x1].absolute
-				 && !(y == y1 && x == x1)
-				 && grid[y1][x1].possible & (1 << i) ) {
-				elsewhere = 1;
-				number = 0;
-			    }
-			}
+		    list_tiles possible = filter_list_tiles(&box_list, NULL, i, TF_TN, filter_is_possible);
+		    Allocs_mem_push(possible.tiles);
+		    if ( possible.length == 0 ) {
+			tile_env *new_t = gen_tile_env(x, y, i, &grid[y][x], grid);
+			if (new_t == NULL) OOM_error();
+			if (enqueue(queue, tile_resolved, new_t) == 0) {
+			    OOM_error();
+			}			
+			break;
 		    }
-		}
-		if ( number ) {
-		    tile_env *new_t = gen_tile_env(x, y, number, &grid[y][x], grid);
-		    if (new_t == NULL) OOM_error();
-		    if (enqueue(queue, tile_resolved, new_t) == 0) {
-			OOM_error();
-		    }
-		    break;
 		}
 	    }
 	}
     }
+    Allocs_pop();
     return 1;
 }
 
@@ -575,54 +721,4 @@ void OOM_error() {
     // Something went wrong and just exit
     printf("Something went wrong. OOM possible\n");
     exit(1);
-}
-
-
-/***********************************************************/
-/* I'm entertaining the idea of just operating on the grid */
-/* as through lists. This code does nothing right now for  */
-/* the program. Its only here as reminder to self that I   */
-/* have this opportunity. 				   */
-/***********************************************************/
-
-typedef struct list_tile {
-    i8 x;
-    i8 y;
-    struct tile* tile;
-}list_tile;
-
-typedef struct list_tiles {
-    u32 length;
-    list_tile *tiles;
-}list_tiles;
-
-// Creates a list of tiles on the heap and returns a pointer
-// List of tiles generated are within the constraints of x and y
-// and applies the function given to determine inclusion
-list_tiles gen_tile_list(u8 lower_y, u8 upper_y, u8 lower_x, u8 upper_x,
-			 u8(*using)(struct tile* tile), struct tile (*grid)[9]);
-
-list_tiles gen_tile_list(u8 lower_y, u8 upper_y, u8 lower_x, u8 upper_x,
-			 u8(*using)(struct tile* tile), struct tile (*grid)[9]) {
-    list_tile list[81] = {0};
-    u8 list_cursor = 0;
-    for (u8 y = lower_y; y < upper_y; y++) {
-	for (u8 x = lower_x; x < upper_x; x++) {
-	    if ( using(&grid[y][x]) ) {
-		list[list_cursor].y = y;
-		list[list_cursor].x = x;
-		list[list_cursor].tile = &grid[y][x];
-		list_cursor++;
-	    }
-	}
-    }
-
-    list_tile *result = NULL;
-    if ( list_cursor >0 ) {
-	result = calloc(list_cursor, sizeof(list_tile));
-	if ( result == NULL )
-	    OOM_error();
-	memcpy(result, list, list_cursor);
-    }
-    return (list_tiles){list_cursor, result};
 }

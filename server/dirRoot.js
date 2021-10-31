3d5893e09fed37a211e620bb57ef2a9b4f2f7620
Nicholas Napoli <38023430+nicknapoli82@users.noreply.github.com>
2021-10-30 20:26:14 -0700

    Update PATHS to accept Windows pathing

--- server/dirRoot.js
+++ server/dirRoot.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
-const PATHS = {
-  server: path.dirname(require.main.filename),
-  root: path.dirname(require.main.filename).split('/').slice(0, -1).join('/'),
-  static: path.dirname(require.main.filename) + '/static'
+const PATHS = () => {
+  if (process.platform === "linux")
+ 	  return { server: path.dirname(require.main.filename),
+	           root: path.dirname(require.main.filename).split('/').slice(0, -1).join('/'),
+		         static: path.dirname(require.main.filename) + '/static'
+	         };
+  else if(process.platform === "win32")
+	  return { server: path.dirname(require.main.filename),
+		         root: path.dirname(require.main.filename).split('\\').slice(0, -1).join('/'),
+		         static: path.dirname(require.main.filename) + '\\static'
+	         };
 };
 
-module.exports = PATHS;
+module.exports = PATHS();
